f67dc6304a7a2363b3bd8c85d430d8351543f435
DaniarKamaev <daniarkamaev@gmail.com>
2025-07-16 13:22:40 -0700

    Initial Commit

---
+++ LuxuryTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuxuryTest
+//
+//  Created by dany on 16.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
